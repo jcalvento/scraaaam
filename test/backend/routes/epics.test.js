0e8c41c3c9ca9ec5e82472ed34a9b50c6e564d3a
Roman Rizzi <rizziromanalejandro@gmail.com>
2017-05-01 21:49:28 -0300

    Endpoints relacionados con la creacion de epics

--- test/backend/routes/epics.test.js
+++ test/backend/routes/epics.test.js
@@ -12,35 +12,35 @@ const expect = chai.expect
 chai.use(chaiAsPromised)
 
 describe("Epic related endpoints", () => {
-  setupMocha()
+	setupMocha()
 
-  describe("GET /epics/:epic", () => {
-    it("Throws and error when the epic is missing", async() => {
-      const unexistentRecordId = 156497
+	describe("GET /epics/:epic", () => {
+		it("Throws and error when the epic is missing", async() => {
+			const unexistentRecordId = 156497
 
-      await get(`/epics/${unexistentRecordId}`, 404)
-    })
+			await get(`/epics/${unexistentRecordId}`, 404)
+		})
 
-    const epicName = 'Test Epic'
+		const epicName = 'Test Epic'
 
-    it("When the epic has a comment attached to it", async() => {
-      const commentBody = 'New comment'
-      const newComment = await Comment.create({ body: commentBody })
-      const epic = await Epic.create({ name: epicName, comments: [newComment] })
+		it("When the epic has a comment attached to it", async() => {
+			const commentBody = 'New comment'
+			const newComment = await Comment.create({ body: commentBody })
+			const epic = await Epic.create({ name: epicName, comments: [newComment] })
 
-      const response = await get(`/epics/${epic._id}`, 200)
-      const comment = response.body.comments[0]
+			const response = await get(`/epics/${epic._id}`, 200)
+			const comment = response.body.comments[0]
 
-      expect(comment.body).to.eq(commentBody)
-    })
+			expect(comment.body).to.eq(commentBody)
+		})
 
-    it("When the epic has a task attached to it", async() => {
-      const taskDescription = 'Task desc'
-      const newTask = await Task.create({ description: taskDescription })
-      const epic = await Epic.create({ name: epicName, tasks: [newTask] })
+		it("When the epic has a task attached to it", async() => {
+			const taskDescription = 'Task desc'
+			const newTask = await Task.create({ description: taskDescription })
+			const epic = await Epic.create({ name: epicName, tasks: [newTask] })
 
-      const response = await get(`/epics/${epic._id}`, 200)
-      const task = response.body.tasks[0]
+			const response = await get(`/epics/${epic._id}`, 200)
+			const task = response.body.tasks[0]
 
       expect(task.description).to.eq(taskDescription)
     })
