c9b88a8be420d3411840437e1fb5d3caf822a231
jcalvento <julian.calvento@10pines.com>
2017-06-03 15:00:34 -0300

    Remove mongo task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,37 +2,31 @@ import gulp from 'gulp'
 import eslint from 'gulp-eslint'
 import babel from 'gulp-babel'
 import uglify from 'gulp-uglify'
-import minify from 'gulp-minify'
 import gls from 'gulp-live-server'
 
-const srcFiles = 'src/**/*.js'
-const backendFiles = 'src/backend/**/*.js'
-const frontendFiles = 'src/frontend/**/*.js'
-const testsFiles = 'test/**/*.js'
-const buildDist = (folderName) => `dist/${folderName}`
+const srcFiles = 'src/**/*.js';
+const backendFiles = 'src/backend/**/*.js';
+const frontendFiles = 'src/frontend/**/*.js';
+const testsFiles = 'test/**/*.js';
+const buildDist = (folderName) => `dist/${folderName}`;
  
 gulp.task('lint', () => {
   return gulp.src([testsFiles, srcFiles])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
-})
+});
 
 gulp.task('transpile', () => {
-  gulp.src(backendFiles).pipe(babel()).pipe(gulp.dest(buildDist('backend')))
+  gulp.src(backendFiles).pipe(babel()).pipe(gulp.dest(buildDist('backend')));
 
   gulp.src(frontendFiles).pipe(babel()).pipe(uglify()).pipe(gulp.dest(buildDist('frontend')))
-})
+});
 
-gulp.task('transpile:watch', () => gulp.watch(srcFiles, ['transpile']))
+gulp.task('transpile:watch', () => gulp.watch(srcFiles, ['transpile']));
 
-gulp.task('mongo:start', () => {
-  var exec = require('child_process').exec
-  exec('mongod --dbpath mongo_data')
-})
-
-gulp.task('start:watch', ['mongo:start', 'transpile:watch'], () => {
-  const server = gls.new(['--', buildDist('backend/server.js')])
+gulp.task('start:watch', ['transpile:watch'], () => {
+  const server = gls.new(['--', buildDist('backend/server.js')]);
   
   server.start()
-})
+});
