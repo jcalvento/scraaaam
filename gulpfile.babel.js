d692181126fc12be897c7c281c4cc7f0d18503e5
julian <julian.calvento@10pines.com>
2017-06-05 14:54:17 -0300

    Add coverage reports to travis

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,6 @@
 import gulp from 'gulp'
 import eslint from 'gulp-eslint'
 import babel from 'gulp-babel'
-import uglify from 'gulp-uglify'
 import gls from 'gulp-live-server'
 import webpackStream from 'webpack-stream'
 import webpack from 'webpack';
@@ -9,10 +8,10 @@ import mocha from 'gulp-mocha';
 
 const srcFiles = 'src/**/*.js';
 const backendFiles = 'src/backend/**/*.js';
-const frontendFiles = 'src/frontend/**/*.js';
 const testsFiles = 'test/**/*.js';
 const buildDist = (folderName) => `dist/${folderName}`;
 const protractor = require("gulp-protractor").protractor;
+const exec = require('child_process').exec;
 let server;
 
 gulp.task('lint', () => {
@@ -25,8 +24,8 @@ gulp.task('lint', () => {
 gulp.task('transpile', () => {
   gulp.src(backendFiles).pipe(babel()).pipe(gulp.dest(buildDist('backend')));
 
-  const webPackEntryPoint = './src/frontend/bootstrap.js'
-  const webPackConfig = require('./webpack.config.js')
+  const webPackEntryPoint = './src/frontend/bootstrap.js';
+  const webPackConfig = require('./webpack.config.js');
   
   return gulp.src(webPackEntryPoint)
     .pipe(webpackStream(webPackConfig, webpack))
@@ -41,7 +40,7 @@ const startServer = function() {
   return server.start()
 };
 
-gulp.task('start:watch', ['transpile', 'transpile:watch'], startServer);
+gulp.task('start:watch', ['transpile:watch'], startServer);
 gulp.task('start', ['transpile'], startServer);
 
 gulp.task('test-backend', () =>
@@ -69,4 +68,6 @@ gulp.task('frontend-all', ['frontend-components', 'frontend-e2e']);
 
 gulp.task('all', ['test-backend', 'frontend-components', 'frontend-e2e']);
 
-gulp.task('all-non-e2e', ['test-backend', 'frontend-components']);
+gulp.task('all-non-e2e', () =>
+  exec('NODE_ENV=test nyc --require babel-core/register mocha --require babel-polyfill --compilers js:babel-register --recursive test/frontend test/backend')
+);
