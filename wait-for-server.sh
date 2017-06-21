18951c5c015966154541e4c0ee5839ab285d1eda
jcalvento <julian.calvento@10pines.com>
2017-06-20 22:26:46 -0300

    Add script to wait for server before running e2e tests.

---
+++ wait-for-server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+shift
+cmd="$@"
+
+until curl -f http://backend:3001; do
+  >&2 echo "Server is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Server is up - executing command"
+exec $cmd
